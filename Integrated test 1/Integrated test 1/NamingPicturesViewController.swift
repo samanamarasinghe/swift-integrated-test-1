053dd64fbf11e09560b80f5be40bb4da76a822d7
saman <saman@44River-saman-iMac.local>
2015-07-12 20:59:21 -0400

    changed the delay

---
+++ Integrated test 1/Integrated test 1/NamingPicturesViewController.swift
@@ -0,0 +1,50 @@
+//
+//  NamingPicturesViewController.swift
+//  Integrated test 1
+//
+//  Created by saman on 7/12/15.
+//  Copyright (c) 2015 saman. All rights reserved.
+//
+
+import UIKit
+
+class NamingPicturesViewController: UIViewController {
+    var imageName = "Comb"
+    //var imageView: UIImageView!
+    
+    //imageView.frame = CGRectMake(0.0, 60.0, view.bounds.height-50, ((view.bounds.height-50)*3/4))
+    
+    
+    @IBAction func correct(sender: AnyObject) {
+        
+    }
+    
+    
+    
+    @IBAction func Incorrect(sender: AnyObject) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //  var imageView = UIImageView(frame:CGRectMake(0.0, 60.0, view.bounds.height-50, ((view.bounds.height-50)*3/4)))
+        
+        var imageView = UIImageView(frame:CGRectMake(0.0, 60.0, view.bounds.height-50, ((view.bounds.height-50)*3/4)))
+        var image = UIImage(named: imageName)
+        imageView.image = image
+        self.view.addSubview(imageView)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.Landscape.rawValue)
+    }
+    
+
+
+}
---
+++ Integrated test 1/Integrated test 1/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Integrated test 1
+//
+//  Created by saman on 7/12/15.
+//  Copyright (c) 2015 saman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //var imageView: UIImageView!
+    
+    //imageView.frame = CGRectMake(0.0, 60.0, view.bounds.height-50, ((view.bounds.height-50)*3/4))
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //  var imageView = UIImageView(frame:CGRectMake(0.0, 60.0, view.bounds.height-50, ((view.bounds.height-50)*3/4)))
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.Landscape.rawValue)
+    }
+    
+    
+}
+
